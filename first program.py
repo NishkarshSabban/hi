6669e90e534cb241c490853330a587a8acf80ab3
NishkarshSabban <nishkarshsabban820@gmail.com>
2025-10-29 23:00:10 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,15 @@
+""""print("hello world")
+
+
+m1=input("enter first fav movie")
+m2=input("enter second fav movie")
+m3=input("enter third fav movie")
+list=[m1,m2,m3]
+
+print(list)"""
+
+
+
+
+import first
+first.add(1,5)
